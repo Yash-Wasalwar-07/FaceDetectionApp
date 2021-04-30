c7950b81adef71fe64b16591d915379dac9da58f
Yash Wasalwar <yash.wasalwar07@hotmail.com>
2021-04-30 17:46:01 +0530

    Initial Commit

---
+++ Face_Detector.py
@@ -0,0 +1,33 @@
+import cv2
+
+# Load some pre-trained data on face frontals from opencv (haar cascade algorithm)
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam
+webcam = cv2.VideoCapture(0)
+
+while True:
+
+    # Read the current frame
+    successful_frame_read, frame = webcam.read()
+
+    # must convert to grayscale
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
+
+    cv2.imshow("Yash Wasalwar's Face Detector", frame)
+    key = cv2.waitKey(1)
+
+    # Stop if Q key is pressed
+    if key == 81 or key == 113:
+        break
+
+# Release the videoCapture Object
+webcam.release()
+print("Code completed")
+
